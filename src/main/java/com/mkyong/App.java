f8c9568eb6b91fb31675094ca9c2d56da0be7d3f
Prashanth Boodithi <prashanthbgoud@gmail.com>
2017-07-17 10:37:46 +0530

    firstbranch

--- src/main/java/com/mkyong/App.java
+++ src/main/java/com/mkyong/App.java
@@ -6,19 +6,19 @@ import java.util.UUID;
  * Generate a unique number
  *
  */
-public class App 
+public class App
 {
 
     public static void main( String[] args )
     {
         App obj = new App();
-        System.out.println("Unique ID : " + obj.generateUniqueKey());
+        System.out.println("Unique ID1 : " + obj.generateUniqueKey());
     }
-    
+
     public String generateUniqueKey(){
-    	
+
     	String id = UUID.randomUUID().toString();
     	return id;
-    	
+
     }
 }
