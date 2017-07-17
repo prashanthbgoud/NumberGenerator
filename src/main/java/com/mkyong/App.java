45c23c9df80553c2b22423b9f08d12eb8b1e9b1b
Prashanth Boodithi <prashanthbgoud@gmail.com>
2017-07-17 10:38:46 +0530

    secondbranch

--- src/main/java/com/mkyong/App.java
+++ src/main/java/com/mkyong/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         App obj = new App();
-        System.out.println("Unique ID1 : " + obj.generateUniqueKey());
+        System.out.println("Unique ID2 : " + obj.generateUniqueKey());
     }
 
     public String generateUniqueKey(){
