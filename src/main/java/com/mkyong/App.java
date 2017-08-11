44e7063905aaf64b4c6228080fc26c4e41452214
Prashanth <prashanthbgoud@gmail.com>
2017-08-11 11:47:33 +0530

    commit

--- src/main/java/com/mkyong/App.java
+++ src/main/java/com/mkyong/App.java
@@ -6,7 +6,7 @@ import java.util.UUID;
  * Generate a unique number
  *
  */
-public class App 
+public class App
 {
 
     public static void main( String[] args )
@@ -14,11 +14,12 @@ public class App
         App obj = new App();
         System.out.println("Unique ID : " + obj.generateUniqueKey());
     }
-    
+
     public String generateUniqueKey(){
-    	
+
     	String id = UUID.randomUUID().toString();
     	return id;
-    	
+      return id;
+
     }
 }
